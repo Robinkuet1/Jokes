f5ec00b14b17edd784da5fbd52e82b2686d3285c
Robin Kuettel <robin.kuettel@roche.com>
2023-05-10 11:09:04 +0200

    fix error in main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import mysql.connector
 db = mysql.connector.connect(host = 'db', user = 'foo', password = 'bar', port = 3306)
 
 
-mycursor = mydb.cursor()
+mycursor = db.cursor()
 mycursor.execute("SELECT * FROM joke")
 myresult = mycursor.fetchall()
 for i in myresult:
