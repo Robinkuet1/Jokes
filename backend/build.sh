892c3b995519215e7b0dc3a617d216a82fcaef32
Robin Kuettel <robin.kuettel@roche.com>
2023-05-10 11:07:05 +0200

    fix error in dockerfile

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,4 @@
-sudo docker kill JokeAPI
-sudo docker rm JokeAPI
-sudo docker build . -t robinkuet1/JokeAPI
-sudo docker run --restart always -p 5678:5678 --link database:db -d --name JokeAPI robinkuet1/JokeAPI
+sudo docker kill jokeapi
+sudo docker rm jokeapi
+sudo docker build . -t robinkuet1/jokeapi
+sudo docker run --restart always -p 5678:5678 --link database:db -d --name jokeapi robinkuet1/jokeapi
