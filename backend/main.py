4ead853560e3524e529210234a973e232a1a6482
Robin Kuettel <robin.kuettel@roche.com>
2023-05-11 10:48:54 +0200

    fixed autocomplete

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 import mysql.connector
 from flask import Flask, request
+from flask_cors import CORS
 import json
 
 db = mysql.connector.connect(host = 'db', user = 'foo', password = 'bar', port = 3306, database = 'jokes')
 cursor = db.cursor()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -26,7 +28,13 @@ def autocompleteTopics():
     searchParam = request.args.get("category")
     cursor.execute(f"SELECT Name FROM category WHERE Name LIKE \"%{searchParam}%\"")
     result = cursor.fetchall()
-    return json.dumps(result)
+    result = cursor.fetchall()
+    maxResult = 50
+    resultList = []
+    for i in result:
+        maxResult += 1
+        resultList.append(i[0])
+    return json.dumps(resultList)
 
 @app.route("/register")
 def register():
@@ -34,6 +42,5 @@ def register():
     pwd = request.args.get("pwd")
     if(uname == "" or pwd == ""):
         return "Error"
-
-
+    
 app.run(host="0.0.0.0",port=5678)
