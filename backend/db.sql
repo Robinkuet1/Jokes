18f671c6e1fc5442290601f2255bfa322237b77a
Robinkuet1 <112063365+Robinkuet1@users.noreply.github.com>
2023-05-22 10:59:29 +0000

    Update API

--- backend/db.sql
+++ backend/db.sql
@@ -8,12 +8,25 @@ create table category
     Name varchar(64) null
 );
 
+create table country
+(
+    Id   int auto_increment
+        primary key,
+    Name varchar(128) null,
+    CODE varchar(2)   null
+);
+
 create table user
 (
-    Id       int auto_increment
+    Id        int auto_increment
         primary key,
-    Username varchar(64)  not null,
-    Password varchar(128) not null
+    Username  varchar(64)  not null,
+    Password  varchar(128) not null,
+    CountryId int          not null,
+    NSFW      tinyint(1)   null,
+    Token     varchar(64)  null,
+    constraint user_country_Id_fk
+        foreign key (CountryId) references country (Id)
 );
 
 create table joke
@@ -23,6 +36,7 @@ create table joke
     Text       varchar(512) null,
     CategoryId int          not null,
     UserId     int          not null,
+    NSFW       tinyint(1)   null,
     constraint Joke_category_Id_fk
         foreign key (CategoryId) references category (Id),
     constraint Joke_user_Id_fk
--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,27 @@ import mysql.connector
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import hashlib
+import random
+import json
+import requests
 
-def sql(querry, size = 10):
+def select(querry, size = 10):
     db = mysql.connector.connect(host = 'db', user = 'foo', password = 'bar', port = 3306, database = 'jokes')
     cursor = db.cursor()
     cursor.execute(querry)
     return cursor.fetchmany(size)
 
+def insert(querry, values):
+    db = mysql.connector.connect(host = 'db', user = 'foo', password = 'bar', port = 3306, database = 'jokes')
+    cursor = db.cursor()
+    cursor.execute(querry, values)
+    db.commit()
+    return cursor.rowcount
+
+def sha256(data):
+    x = hashlib.sha256(str(data).encode("UTF-8"))
+    return x.hexdigest()
 
 app = Flask(__name__)
 CORS(app)
@@ -29,7 +43,7 @@ def jokes():
 @app.route("/autocomplete/topics")
 def autocompleteTopics():
     searchParam = request.args.get("category")
-    result = sql(f"SELECT Name FROM category WHERE Name LIKE \"%{searchParam}%\"")
+    result = select(f"SELECT Name FROM category WHERE Name LIKE \"%{searchParam}%\"")
     maxResult = 50
     resultList = []
     for i in result:
@@ -40,15 +54,30 @@ def autocompleteTopics():
 @app.route("/register")
 def register():
     uname = request.args.get("username")
-    pwd = request.args.get("pwd")
+    pwd = request.args.get("password")
+    nsfw = request.args.get("nsfw")
+    if(uname == None or pwd == None or nsfw == None):
+        return "Not all required parameters provided"
+    token = sha256(uname + pwd + str(random.randint(10000000,99999999999)))
     if(uname == "" or pwd == ""):
         return "Error"
-    
-    
+    exists = select(f"SELECT Id FROM user WHERE Username = '{uname}'")
+    if(len(exists) != 0):
+        return "User already exists"
+    countryCode = requests.get(f"http://ip-api.com/json/{request.remote_addr}").text
+    result = insert(f"INSERT INTO user (Username, Password, Token, CountryId, NSFW) VALUES (%s, %s, %s, %s, %s)", (uname, pwd, token, 1, nsfw))
+    return str(result)
+
 @app.route("/login")
 def login():
     uname = request.args.get("username")
-    pwd = request.args.get("pwd")
-    exists = sql(f"SELECT Id FROM user WHERE Username = '{uname}'")
+    pwd = request.args.get("password")
+    exists = select(f"SELECT Id FROM user WHERE Username = '{uname}'")
+    if(len(exists) == 0):
+        return "User not found"
+    token = select(f"SELECT Token FROM user WHERE Username = '{uname}' AND Password = '{pwd}'")
+    if(len(token) == 0):
+        return "Unauthorized"
+    return token[0][0]
 
 app.run(host="0.0.0.0",port=5678)
